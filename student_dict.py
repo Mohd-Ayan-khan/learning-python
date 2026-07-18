f1568c7b6ec5415b720cf803755c38f0e846208f
Mohd-Ayan-khan <ayansolankiayansolanki303@gmail.com>
2026-07-18 16:48:46 +0530

    student dictionary

---
+++ student_dict.py
@@ -0,0 +1,15 @@
+student={}
+
+student["id"]=(int(input("enter your id :")))
+student["name"]=(input("enter your Name :"))
+student["address"]=(input("enter your Address :"))
+student["email"]=(input("enter your Email :"))
+student["Contact"]=(int(input("enter your contact Number :")))
+
+print(student)
+
+study=[(input("enter your qualification : "))]
+
+student["qualification"]=study
+
+print(student)
