55e2a739e3ecbb003d3bb45891e67f76b8a09b07
Mohd-Ayan-khan <ayansolankiayansolanki303@gmail.com>
2026-07-29 16:24:00 +0530

    check type of variable

---
+++ test.py
@@ -0,0 +1,23 @@
+id = (int(input("enter your id :")))
+name = (str(input("enter your name :")))
+age = (int(input("enter your age :")))
+city = (str(input("enter your city :")))
+height = (float(input("enter your height :")))
+
+print(id)
+print(type(id))
+
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(city)
+print(type(city))
+
+print(height)
+print(type(height))
+
+
