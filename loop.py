131fb5202126d5edd457aa68b8dd8dd9606f0210
Mohd-Ayan-khan <ayansolankiayansolanki303@gmail.com>
2026-07-29 16:14:25 +0530

    print list using loop with number

---
+++ loop.py
@@ -0,0 +1,7 @@
+datalist=["ayan","awesh","rehan","afrin","ikra"]
+number=["first","second","third","forth","five"]
+
+length=(len(datalist))
+
+for i in range(length):
+    print(f"{number[i]} = {datalist[i]}")
